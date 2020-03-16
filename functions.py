53dd5d42e03e34389d175420da7f38a577faa551
trystep <43743116+trystep@users.noreply.github.com>
2020-03-16 06:10:16 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 
 
 # Отлов ошибок
@@ -14,24 +15,89 @@ def log_error(f):
 
 
 # Определение времени суток для приветсвенного сообщения
-def times_of_day():
+def times_of_day(dt=None, ts=None, tod_dict=None):
     '''
-        Определение времени суток.
-        Утро: с 5.00 до 12.00 включительно
-        День: с 12.00 до 18.00 включительно
-        Вечер: с 18.00 до 22.00 включительно
-        Ночь: с 22.00 до 5.00 включительно
+    Принимает объект datetime (dt) или timestamp (ts), и словарь tod_dict {час : наименование времени}
+    Возвращает строку c приветственным сообщением и временем суток.
+
+    При отсутствии аргументов - возвращает строку с приветственным сообщением,
+    где первый элемент "Добрый", а второй элемент соответствует времени суток, то есть утро, день, вечер и ночь
+    в зависимости от времени
+
+        «утро» — с 0.00 до 12.00,
+        «день» — с 12.00 до 18.00,
+        «вечер» — с 18.00  до 21.00,
+        «ночь» - с 21.00  до 0.00,
+
+    Eсли час не отражен в словаре, возвращает None
+    Требует import datetime.
+
+
     '''
-    time_list = time.localtime()
-    hours = time_list[3]
-    if 5 < int(hours) <= 12:
-        return "Доброе утро!"
-    elif 12 < int(hours) <= 18:
-        return "Добрый день!"
-    elif 18 < int(hours) <= 22:
-        return "Добрый вечер!"
+    morning = "Доброе утро!"
+    day = "Добрый день!"
+    evening = "Добрый вечер!"
+    night = "Доброй ночи!"
+
+    # определение словаря
+    if tod_dict:
+        pass
     else:
-        return "Доброй ночи!"
+        tod_dict = {0: morning,
+                    1: morning,
+                    2: morning,
+                    3: morning,
+                    4: morning,
+                    5: morning,
+                    6: morning,
+                    7: morning,
+                    8: morning,
+                    9: morning,
+                    10: morning,
+                    11: morning,
+                    12: day,
+                    13: day,
+                    14: day,
+                    15: day,
+                    16: day,
+                    17: day,
+                    18: evening,
+                    19: evening,
+                    20: evening,
+                    21: night,
+                    22: night,
+                    23: night}
+
+    # определение оцениваемого времени
+    if dt:
+        if isinstance(dt, datetime.datetime):
+            dt = dt
+        else:
+            print('некорректный формат dt: %s \n требуется объект datetime.datetime' % (type(dt)))
+            return None
+    elif ts:
+        if isinstance(ts, (int, float)):
+            if ts > 0:
+                dt = datetime.datetime.fromtimestamp(ts)
+                print(dt.ctime())
+            else:
+                print('отрицательное значение ts недопустимо')
+                return None
+        else:
+            print('некорректный формат ts: %s \n требуется значение int или float' % (type(ts)))
+            return None
+    else:
+        dt = datetime.datetime.now()
+    h = dt.hour
+
+    # подбор
+    if h in tod_dict:
+        tod = tod_dict[h]
+        return tod
+    else:
+        print('Значение времени отсутствует в словаре')
+        return None
+
 
 
 ###############################################################################################
